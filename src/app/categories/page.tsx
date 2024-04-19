0bf339a4d266fe96ada338b7a68701bafbdebeeb
BGabrielius <gabrieliusbatakis@gmail.com>
2024-04-19 21:27:05 +0300

    catagories page implemantation

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Category from '@/components/Category';
+import UtilButton from '@/components/UtilButton';
+import { useRouter } from 'next/navigation';
+import data from '../../data.json';
+
+import React from 'react';
+// const fetchData = () => {
+//   const data = fetch('./data.json').then((results) => results.json());
+//   return data;
+// };
+
+const page = () => {
+  const router = useRouter();
+  return (
+    <main className='flex flex-col items-center'>
+      <section className='w-full flex justify-between items-center mb-20 md:mb-14'>
+        <UtilButton action={() => router.replace('/')} type='sm-back' />
+        <h2 className='stroke-home bg-gradient-to-b from-gradient-text to-white text-gradient-text text-transparent bg-clip-text text-[48px] md:text-[104px] 2xl:text-f-heading-xl'>
+          Pick a Category
+        </h2>
+
+        <div className='hidden sm:block'></div>
+      </section>
+      <section className='grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 md:gap-8 md:max-w-[700px] 2xl:grid-cols-3 2xl:gap-x-[420px]'>
+        {data &&
+          Object.keys(data.categories).map((category: any) => (
+            <Category
+              key={category}
+              action={() => router.push(`/categories/${category}`)}
+              category={category}
+            />
+          ))}
+      </section>
+    </main>
+  );
+};
+
+export default page;
