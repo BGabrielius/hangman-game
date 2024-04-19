56f4ae4654847914409f5522a30a00592792d6d6
BGabrielius <gabrieliusbatakis@gmail.com>
2024-04-19 21:29:05 +0300

    accessability tweaks/style

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -4,9 +4,7 @@ interface Props {
   category: string;
   action: () => void;
 }
-const StyledCategory = styled.li`
-  border-radius: 20px;
-
+const StyledCategory = styled.button`
   &:hover {
     cursor: pointer;
     opacity: 0.75;
@@ -16,8 +14,9 @@ const StyledCategory = styled.li`
 const Category: React.FC<Props> = ({ category, action }) => {
   return (
     <StyledCategory
-      className='bg-c-blue text-white shadow-s-primary w-[324px] py-5 text-center text-[24px] md:text-f-heading-m md:py-[55px] 2xl:w-[384px] 2xl:py-[59px]'
-      onClick={() => action}
+      className='bg-c-blue text-white shadow-s-primary w-[324px] py-5 text-center text-[24px] rounded-[20px] md:rounded-[40px] md:text-f-heading-m md:py-[55px] 2xl:w-[384px] 2xl:py-[59px]'
+      onClick={action}
+      tabIndex={0}
     >
       {category}
     </StyledCategory>
