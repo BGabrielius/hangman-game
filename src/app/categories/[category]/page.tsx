4bf9f38836fd9a28bd461cf090dd6a5f7abf8d7e
BGabrielius <gabrieliusbatakis@gmail.com>
2024-04-20 21:30:25 +0300

    adjusted styles/added ingame section

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -9,6 +9,7 @@ import HealthBar from '@/components/HealthBar';
 import { Heart } from '@/components/svg';
 import styled from 'styled-components';
 import Modal from '@/components/Modal';
+import Letter from '@/components/Letter';
 
 const StyledTopBar = styled.div`
   display: flex;
@@ -26,17 +27,18 @@ const StyledTopBar = styled.div`
 const page = () => {
   const params = useParams<{ category: string }>();
 
-  // variables
-
   const [isMounted, setIsMounted] = useState<boolean>(false);
   const [category, _setCategory] = useState<string>(
     params.category.replace('%20', ' ')
   );
-  const [unknownWord, setUnknownWord] = useState<any>();
 
+  const [unknownWord, setUnknownWord] = useState<any>();
+  const alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
   // modal
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [modalHeadline, setModalHeadline] = useState<string>('Paused');
+  const [modalHeadline, setModalHeadline] = useState<
+    'Paused' | 'You Win' | 'You Lose'
+  >('Paused');
 
   useEffect(() => {
     if (unknownWord) console.log('kek', unknownWord);
@@ -52,13 +54,13 @@ const page = () => {
         data.categories[category as keyof typeof data.categories];
       const selected = categoryData[Math.floor(30 * Math.random())];
       selected.selected = true;
-      setUnknownWord(selected.name.split(''));
+      setUnknownWord(selected.name.toUpperCase().split(''));
     }
     return;
   };
   return (
-    <main className='w-full flex flex-col items-center justify-center'>
-      <section className='w-full flex justify-between items-center'>
+    <main className='w-full flex flex-col items-center justify-between'>
+      <section className='mb-20 w-full flex justify-between items-center'>
         <StyledTopBar>
           <UtilButton type='sm-menu' action={() => setShowModal(true)} />
           <h3 className='text-white text-[40px] md:text-f-heading-m 2xl:text-f-heading-l'>
@@ -73,16 +75,40 @@ const page = () => {
         </StyledTopBar>
       </section>
 
-      <section>
-        {showModal && (
-          <Modal
-            headline={modalHeadline}
-            openModal={showModal}
-            closeModal={() => setShowModal(false)}
-          />
-        )}
+      {showModal && (
+        <Modal
+          headline={modalHeadline}
+          openModal={showModal}
+          closeModal={() => setShowModal(false)}
+        />
+      )}
+      <section className='h-full flex flex-col items-center justify-around gap-28'>
+        <div className='h-full w-full flex flex-wrap items-center justify-center gap-2 md:gap-5 2xl:gap-6'>
+          {unknownWord &&
+            unknownWord.map((letter: string, index: number) => (
+              <Letter
+                action={() => undefined}
+                letter={letter}
+                type='primary'
+                key={letter + index}
+              />
+            ))}
+        </div>
+        <div className='h-full w-full grid grid-cols-9 gap-y-6 gap-x-2 md:gap-x-4 2xl:gap-x-6'>
+          {alphabet &&
+            alphabet
+              .toUpperCase()
+              .split('')
+              .map((letter: string, index: number) => (
+                <Letter
+                  action={() => undefined}
+                  letter={letter}
+                  type='secondary'
+                  key={letter + index}
+                />
+              ))}
+        </div>
       </section>
-      <section></section>
     </main>
   );
 };
--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -11,11 +11,14 @@ const StyledLetter = styled.li``;
 const Letter: React.FC<Props> = ({ letter, action, type }) => {
   return (
     <StyledLetter
-      className={
-        type === 'primary'
-          ? 'text-[40px] px-3 py-1 bg-c-blue text-white shadow-s-primary rounded-[40px] md:text-[64px] md:px-8 md:py-2 2xl:text-f-heading-l 2xl:px-10 2xl:py-0'
-          : 'text-[24px] p-[10px] bg-white text-c-darknavy rounded-[24px] md:text-f-heading-m md:px-6 md:py-[6px] 2xl:px-[46px] hover:bg-c-blue hover:text-white hover:cursor-pointer'
-      }
+      className={` 
+        ${
+          letter === ' '
+            ? 'bg-transparent shadow-none text-[40px] px-3 py-1 rounded-[40px] md:text-[64px] md:px-8 md:py-2 2xl:text-f-heading-l 2xl:px-10 2xl:py-0'
+            : type === 'primary'
+            ? 'font-bold text-[40px] px-2 py-1 bg-c-blue text-white shadow-s-primary rounded-[12px] md:rounded-[32px] 2xl:rounded-[40px] md:text-[64px] md:px-8 md:py-2 2xl:text-f-heading-l 2xl:px-10 2xl:py-0'
+            : 'm-[auto] text-[24px] p-[10px] bg-white text-c-darknavy rounded-[8px] md:rounded-[24px] md:text-f-heading-m md:px-6 md:py-[6px] 2xl:px-[46px] hover:bg-c-blue hover:text-white hover:cursor-pointer'
+        }`}
       onClick={() => action}
     >
       {letter}
