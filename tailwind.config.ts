479fe9abc376239800d993881a4a342c5a61c1ed
BGabrielius <gabrieliusbatakis@gmail.com>
2024-04-23 21:16:15 +0300

    additional variable

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
     extend: {
       screens: {
         '2xl': '1440px',
-        sm: '450px',
+        sm: '475px',
       },
       colors: {
         'c-darknavy': '#261676',
@@ -30,6 +30,8 @@ const config: Config = {
         's-util-play': '0 8px 0 7px #9D2DF5, 0 12px 0 11px #243041',
         's-util-lg': '0 5px 0 6px rgba(157, 45, 245, 0.75)',
         's-util-sm': '0 4px 0 1px rgba(157, 45, 245, 0.75)',
+        's-transparent':
+          '0 -3px 0px 3px rgba(60, 116, 255, 0.5), 0 -1px 0 6px rgba(20, 14, 102, 0.75)',
       },
     },
     fontSize: {
